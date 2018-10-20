1efcd7badbebc55b8449dc537cf8319609f7d14d
Abiel Villegas <abiel.villegas17@tectijuana.edu.mx>
2018-10-19 21:22:15 -0700

    Ultimas dos practicas

---
+++ P9Fragments/app/src/main/java/com/example/abiel/p9fragments/ArticleFragment.java
@@ -0,0 +1,15 @@
+package com.example.abiel.p9fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ArticleFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState){
+        View view = inflater.inflate(R.layout.article_fragment_layout, container, false);
+        return view;
+    }
+}
