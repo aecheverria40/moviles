1b4eb7457d3ac36d2c426c06d65b3a2e559e91c2
Abiel Villegas <abiel.villegas17@tectijuana.edu.mx>
2018-10-23 18:18:14 -0700

    Practica 11

--- P10RunetimeFragment/app/src/main/java/com/example/abiel/p10runetimefragment/FirstFragment.java
+++ P10RunetimeFragment/app/src/main/java/com/example/abiel/p10runetimefragment/FirstFragment.java
@@ -6,6 +6,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.sql.BatchUpdateException;
 
 
 /**
@@ -13,6 +16,7 @@ import android.view.ViewGroup;
  */
 public class FirstFragment extends Fragment {
 
+    private Button button;
 
     public FirstFragment() {
         // Required empty public constructor
@@ -23,7 +27,18 @@ public class FirstFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
+
+        button = view.findViewById(R.id.b2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.fragmentManager.beginTransaction().replace(R.id.fragment_container, new SecondFragment(), null).addToBackStack(null).commit();
+            }
+        });
+
+        return view;
     }
 
 }
--- P10RunetimeFragment/app/src/main/java/com/example/abiel/p10runetimefragment/HomeFragment.java
+++ P10RunetimeFragment/app/src/main/java/com/example/abiel/p10runetimefragment/HomeFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -13,6 +14,7 @@ import android.view.ViewGroup;
  */
 public class HomeFragment extends Fragment {
 
+    private Button button;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -23,7 +25,17 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        button = view.findViewById(R.id.b1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                MainActivity.fragmentManager.beginTransaction().replace(R.id.fragment_container, new FirstFragment(), null).addToBackStack(null).commit();;
+            }
+        });
+
+        return view;
     }
 
 }
