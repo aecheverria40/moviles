a8c8d2cfc6fe119e6df164323e9ba5dbf25cb8ef
Abiel Villegas <abiel.villegas17@tectijuana.edu.mx>
2018-10-23 18:43:53 -0700

    Practica 11 23/oct/18

--- P11FragmentActivityCommunication/app/src/main/java/com/example/abiel/p11fragmentactivitycommunication/MainActivity.java
+++ P11FragmentActivityCommunication/app/src/main/java/com/example/abiel/p11fragmentactivitycommunication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.abiel.p11fragmentactivitycommunication;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (findViewById(R.id.frame_container) != null){
+            if (savedInstanceState != null){
+                return;
+            }
+            //Crecion de objeto del fragmento
+            MessageFragment messageFragment = new MessageFragment();
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction().
+                    add(R.id.frame_container, messageFragment, null);
+            fragmentTransaction.commit();
+        }
+
     }
 }
